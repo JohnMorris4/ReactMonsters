b85047d14f5cec8cabe6ab66d1c7e322982c5b5e
johnmorris4 <johnmorris4@bellsouth.net>
2020-08-23 04:08:31 -0400

    Updates

---
+++ precache-manifest.c427002485e4edf9193f2875b69c7939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3487a094d4a914f2463a0a0203655810",
+    "url": "/ReactMonsters/index.html"
+  },
+  {
+    "revision": "135d68655efb3ede91e4",
+    "url": "/ReactMonsters/static/css/main.982c8411.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/ReactMonsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMonsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135d68655efb3ede91e4",
+    "url": "/ReactMonsters/static/js/main.73a19e4d.chunk.js"
+  },
+  {
+    "revision": "1d63a17cd8a577e1aa20",
+    "url": "/ReactMonsters/static/js/runtime-main.8bc2d215.js"
+  }
+]);
